2d861f9f839b6dfc5f92bbe932071cdd03522836
Raúl <raulruizbueno@gmail.com>
2017-08-17 20:59:23 +0200

    up

---
+++ BallTrackV2.py
@@ -0,0 +1,95 @@
+# import the necessary packages
+from picamera.array import PiRGBArray     #As there is a resolution problem in raspberry pi, will not be able to capture frames by VideoCapture
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+import time
+import cv2
+import cv2.cv as cv
+import numpy as np
+
+
+#Image analysis work
+def segment_colour(frame):    #returns only the red colors in the frame
+    hsv_roi =  cv2.cvtColor(frame, cv2.cv.CV_BGR2HSV)
+    mask_1 = cv2.inRange(hsv_roi, np.array([160, 160,10]), np.array([190,255,255]))
+    ycr_roi=cv2.cvtColor(frame,cv2.cv.CV_BGR2YCrCb)
+    mask_2=cv2.inRange(ycr_roi, np.array((0.,165.,0.)), np.array((255.,255.,255.)))
+
+    mask = mask_1 | mask_2
+    kern_dilate = np.ones((8,8),np.uint8)
+    kern_erode  = np.ones((3,3),np.uint8)
+    mask= cv2.erode(mask,kern_erode)      #Eroding
+    mask=cv2.dilate(mask,kern_dilate)     #Dilating
+    #cv2.imshow('mask',mask)
+    return mask
+
+def find_blob(blob): #returns the red colored circle
+    largest_contour=0
+    cont_index=0
+    contours, hierarchy = cv2.findContours(blob, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    for idx, contour in enumerate(contours):
+        area=cv2.contourArea(contour)
+        if (area >largest_contour) :
+            largest_contour=area
+           
+            cont_index=idx
+            #if res>15 and res<18:
+            #    cont_index=idx
+                              
+    r=(0,0,2,2)
+    if len(contours) > 0:
+        r = cv2.boundingRect(contours[cont_index])
+       
+    return r,largest_contour
+
+def target_hist(frame):
+    hsv_img=cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+   
+    hist=cv2.calcHist([hsv_img],[0],None,[50],[0,255])
+    return hist
+
+#CAMERA CAPTURE
+#initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (160, 120)
+camera.framerate = 16
+rawCapture = PiRGBArray(camera, size=(160, 120))
+ 
+# allow the camera to warmup
+time.sleep(0.001)
+ 
+# capture frames from the camera
+for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+      #grab the raw NumPy array representing the image, then initialize the timestamp and occupied/unoccupied text
+      frame = image.array
+      frame=cv2.flip(frame,1)
+      global centre_x
+      global centre_y
+      centre_x=0.
+      centre_y=0.
+      hsv1 = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+      mask_red=segment_colour(frame)      #masking red the frame
+      loct,area=find_blob(mask_red)
+      x,y,w,h=loct
+     
+      
+             
+      if (w*h) < 10:
+            found=0
+      else:
+            found=1
+            simg2 = cv2.rectangle(frame, (x,y), (x+w,y+h), 255,2)
+            centre_x=x+((w)/2)
+            centre_y=y+((h)/2)
+            cv2.circle(frame,(int(centre_x),int(centre_y)),3,(0,110,255),-1)
+            centre_x-=80
+            centre_y=6--centre_y
+            print centre_x,centre_y
+      initial=400
+      flag=0
+      
+      cv2.imshow("draw",frame)    
+      rawCapture.truncate(0)  # clear the stream in preparation for the next frame
+         
+      if(cv2.waitKey(1) & 0xff == ord('q')):
+            break
